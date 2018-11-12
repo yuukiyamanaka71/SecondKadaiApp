30157272e21b1b3f9add7b4ae3c5876a275fb5d1
yuukiyamanaka71 <y.yamanaka71@gmail.com>
2018-11-13 02:42:39 +0900

    課題を作成した

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 山中祐樹 on 2018/11/05.
+//  Copyright © 2018 山中祐樹. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+  
+   
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var name:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        label.text = "こんにちは、\(name)さん"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var name: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.name = name.text!
+        
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
     }
 
 
